d96c17ea6db570ca9adb5a61c880cedd938e6358
jeremi <jeremi.araszkiewicz@gmail.com>
2024-05-23 13:10:44 +0200

    added the raw code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
 
+
+void swap1(int *a, int *b){
+    printf("####BEGIN SWAP FUNCTION\n\n");
+
+    printf("Before SWAP:\n",*a);
+    printf("a= %d\n",*a);
+    printf("b=%d\n",*b);
+
+    int temp =*a;
+    *a=*b;
+    *b=temp;
+
+    printf("\n");
+    printf("After SWAP:\n",*a);
+    printf("a= %d\n",*a);
+    printf("b=%d\n",*b);
+    printf("#### END SWAP FUNCTION\n\n");
+}
+
+
+void swap2(int a, int b){
+    printf("####BEGIN SWAP FUNCTION\n\n");
+
+    printf("Before SWAP:\n",a);
+    printf("a= %d\n",a);
+    printf("b=%d\n",b);
+
+    int temp =a;
+    a=b;
+    b=temp;
+
+    printf("\n");
+    printf("After SWAP:\n",a);
+    printf("a= %d\n",a);
+    printf("b=%d\n",b);
+    printf("#### END SWAP FUNCTION\n\n");
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    printf("SWAP EXAMPLE\n\n");
+
+
+    int a=2;
+    int b=5;
+    printf("Before SWAP:\n",a);
+    printf("a= %d\n",a);
+    printf("b=%d\n",b);
+
+
+
+    printf("\n");
+    printf("After SWAP:\n",a);
+    printf("a= %d\n",a);
+    printf("b=%d\n",b);
+
     return 0;
 }
